dcfb96ff29f0ee854281be340390dc74d0c40796
Borah Lim <borahl@ocf.berkeley.edu>
2019-03-24 03:40:27 -0700

    separated functions and outlined necessary tasks

--- AvalonGame.py
+++ AvalonGame.py
@@ -10,22 +10,70 @@ class AvalonGame:
     def game_start(self, players):
         ruleset = len(players)
         self.roles = rule(ruleset)
-    async def loadrules(self):
+
+    async def loadBoard(self):
 	       playersnamestring = "|"
 	       for x in playerlist:
-		       playersnamestring += "` "+x.name+" `|"
+		     playersnamestring += "` "+x.name+" `|"
 	       if playerno == 5:
 		        self.quest.append(2) #quest 1
 		        self.quest.append(3)
 		        self.quest.append(2)
 		        self.quest.append(3)
 		        self.quest.append(3)
-		        self.roles["Merlin"] = ""
-		        self.roles["The Assassin"] = ""
-		        self.roles["Tenant farmer, Loyal Servant of Arthur"] = ""
-		        self.roles["Omegalul Loyal Servant of Arthur"] = ""
-		        self.roles["Dat boi, Minion of Mordred"] = ""
-		   await client.send_message(message.channel, startStr.format(playersnamestring,len(playerlist),"3","2",rolesStr))
+
+            elif playerno == 6:
+                self.quest.append(2) #quest 1
+		        self.quest.append(3)
+		        self.quest.append(4)
+		        self.quest.append(3)
+		        self.quest.append(4)
+
+            elif playerno == 7:
+                self.quest.append(2) #quest 1
+		        self.quest.append(3)
+		        self.quest.append(3)
+		        self.quest.append(4)
+		        self.quest.append(4)
+
+            elif playerno <= 10 and playerno >= 8:
+                self.quest.append(3) #quest 1
+		        self.quest.append(4)
+		        self.quest.append(4)
+		        self.quest.append(5)
+		        self.quest.append(5)
+            else:
+                return "Invalid Number of Players"
+                await client.send_message(message.channel, startStr.format
+                (playersnamestring,len(playerlist),"3","2",rolesStr))
+
+    async def chooseRoles(self):
+        #should ask the player for what roles they want to play with
+            return
+
+    async def loadRoles(self, roleList):
+        self.roles["Merlin"] = ""
+        self.roles["The Assassin"] = ""
+
+        if PercAndMorg:
+            self.roles["Percival"] = ""
+            self.roles["Morgana"] = ""
+
+        elif Mordred:
+            self.roles["Mordred"] = ""
+
+        elif Oberon:
+            self.roles["Oberon"] = ""
+
+        elif Norebo:
+            self.roles["Norebo"] = ""
+
+        #make function/case to create all good/bad normies
+
+        else:
+            pass
+
+
     async def night(self):
 	       await client.send_message(message.channel, nighttimemsg)
            roleshuffle = copy.deepcopy(playerlist)
